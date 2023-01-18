ddbb52fdb5ad7990cf90509309f24d6b8fab0a1f
Walid1506 <aliwalid97600@gmail.com>
2023-01-18 09:54:29 +0100

    walid

---
+++ parking.py
@@ -0,0 +1,74 @@
+import requests
+from lxml import etree
+import shutil
+import time
+
+
+parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT',
+'FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_CAS_SABL','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_CAS_VICA','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY']
+
+
+def parking(parkings):
+    lien = "https://data.montpellier3m.fr/sites/default/files/ressources/"+parkings+".xml"
+
+    donnee=requests.get(lien)
+   
+    return donnee.text
+
+print(parking('FR_MTP_ANTI'))
+
+
+
+x=0
+while x!=2:
+    for i in parkings:
+        nom=i+".txt"
+        print(parking(i))
+        f1=open(nom,"a", encoding='utf8')
+        f1.write(parking(i))
+    #for i in range (x+1):
+    
+        time.sleep(5)
+
+    #nom= parkings
+
+    #tree = etree.parse(nom)
+        
+    #x = i+1
+#print('fin')
+
+"""
+    tree = etree.parse(nom)
+    for user in tree.xpath("Status"):
+        print('Info Parking :',user.text)
+
+    for name in tree.xpath("Name"):
+
+        pass
+
+    for total in tree.xpath("Total"):
+
+        pass
+
+    for user in tree.xpath("Free"): 
+
+        pass
+
+    for status in tree.xpath("Status"):
+
+        pass
+
+
+
+    f2.write(name.text+ status.text+ user.text+"\n")
+    f2.close()
+    return donnee.text
+
+
+
+
+"""
+
+
+    
+
